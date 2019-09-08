2a50f691f4accaf8eb6811ed0ff02579fbbdbba6
michael <mikelkarije@gmail.com>
2019-09-08 20:11:39 +0300

    the commit adds routing to the app

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,9 @@ from flask import Flask
 # Initializing application
 app = Flask(__name__)
 
-from app import views
+@app.route('/')
+def index():
+    return 'welcome to B.news'
+
+if __name__=="__main__":
+    app.run(debug=True)
