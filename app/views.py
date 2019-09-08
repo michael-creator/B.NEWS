1f7b914bdd5a238d0789da72c90439f9e52e2a3d
michael <mikelkarije@gmail.com>
2019-09-08 21:24:13 +0300

    this commit adds our API key to the application

--- app/views.py
+++ app/views.py
@@ -3,10 +3,11 @@ from app import app
 
 # Views
 @app.route('/news/<int:news_id>')
-def news(news_id):
+def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-title = "Home -welcome the best news site for highlits website"
-    return render_template('news.html',id = news_id)
+
+    title = 'Home - Welcome to The best news Review Website Online'
+    return render_template('index.html', title = title)
