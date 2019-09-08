eaae7b3274fdd54167a910f07ed464a7d9becdcf
michael <mikelkarije@gmail.com>
2019-09-08 20:41:47 +0300

    i have changed some words B.news to news to simplify the work

--- app/views.py
+++ app/views.py
@@ -2,11 +2,11 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
-def index():
+@app.route('/news/<int:news_id>')
+def news(news_id):
 
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'welcome to B.news'
-    return render_template('index.html',message = message)
+
+    return render_template('news.html',id = news_id)
