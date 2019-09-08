3d36377d59bb4a483bd38f08387f727d733d5bd2
michael <mikelkarije@gmail.com>
2019-09-08 20:31:39 +0300

    i have added flask into the project

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
 
+
 # Initializing application
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'welcome to B.news'
-
-if __name__=="__main__":
-    app.run(debug=True)
+from app import views
--- app/views.py
+++ app/views.py
@@ -9,4 +9,4 @@ def index():
     View root page function that returns the index page and its data
     '''
     message = 'welcome to B.news'
-    return render_template('index.html',message = welcome to B.news)
+    return render_template('index.html',message = message)
