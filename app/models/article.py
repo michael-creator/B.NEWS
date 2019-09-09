0af5a247df523aee62988fbba43de3d075356ce3
michael <mikelkarije@gmail.com>
2019-09-09 10:51:16 +0300

    the commit adds the article_id,author,title,description,url,urlToImage,publishedAt,content

---
+++ app/models/article.py
@@ -0,0 +1,10 @@
+class article:
+    def __init__(self,article_id,author,title,description,url,urlToImage,publishedAt,content):
+        self.article_id = article_id
+        self.author = author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+       
---
+++ app/models/source.py
@@ -0,0 +1,5 @@
+class source:
+    def __init__(self,source_id,name):
+        self.source_id = source_id
+        self.name = name
+       
