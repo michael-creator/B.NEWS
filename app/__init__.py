4d5a414e3944c02d2159d45f19dd0472f17656f3
michael <mikelkarije@gmail.com>
2019-09-09 09:13:11 +0300

    addition of blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,21 @@ app = Flask(__name__,instance_relative_config = True)
 
 #setting up configuration
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
 
 from app import views
