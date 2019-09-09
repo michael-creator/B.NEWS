0bfa334558a7ba82b32dc3a559b6597cef36122c
michael <mikelkarije@gmail.com>
2019-09-09 09:07:45 +0300

    i have moved the errors and views file into the main folder

--- app/main/views.py
+++ app/main/views.py
@@ -5,9 +5,4 @@ from app import app
 @app.route('/news/<int:news_id>')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    title = 'Home - Welcome to The best news Review Website Online'
     return render_template('index.html', title = title)
